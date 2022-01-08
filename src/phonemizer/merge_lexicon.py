0b9a379431e13508f4e7a6bbaa4f5b6009c376e3
andompesta <ando.cavallari@gmail.com>
2022-01-08 18:52:25 +0100

    add seq2seq phonemizer based on G2P numpy RNN model

--- src/phonemizer/merge_lexicon.py
+++ src/phonemizer/merge_lexicon.py
@@ -1,11 +1,12 @@
 import os
 import re
 
-
 if __name__ == "__main__":
     lexicon = dict()
 
-    with open(os.path.join("./lexicon", "librispeech-lexicon.txt"), "r", encoding="utf8") as f:
+    with open(
+        os.path.join("./lexicon", "librispeech-lexicon.txt"), "r", encoding="utf8"
+    ) as f:
         for line in f:
             temp = re.split(r"\s+", line.strip("\n"))
             word = temp[0]
@@ -18,7 +19,7 @@ if __name__ == "__main__":
     with open(os.path.join("./lexicon", "homograph-en.txt"), "r", encoding="utf8") as f:
         for line in f.read().splitlines():
             if line.startswith("#"):
-                continue    # comment
+                continue  # comment
 
             word, phones, pron2, pos1 = line.strip().split("|")
             phones = re.split(r"\s+", phones.strip("\n"))
@@ -28,14 +29,10 @@ if __name__ == "__main__":
             else:
                 already_present += 1
             count += 1
-    
+
     print(already_present, count)
     print(len(lexicon))
-    
+
     with open("lexicon.txt", "w") as f:
         for word, phones in lexicon.items():
-            f.write("{}\t{}\n".format(
-                word,
-                " ".join(phones)
-            ))
-    
+            f.write("{}\t{}\n".format(word, " ".join(phones)))
---
+++ src/phonemizer/seq2seq_model.py
@@ -0,0 +1,89 @@
+from torch import nn
+import torch
+
+
+class Seq2Seq(nn.Module):
+    def __init__(
+        self,
+        in_embeddng_size=29,
+        hidden_size=256,
+        out_embedding_size=74,
+    ):
+        super(Seq2Seq, self).__init__()
+        self.in_embeddng_size = in_embeddng_size
+        self.hidden_size = hidden_size
+        self.out_embedding_size = out_embedding_size
+        self.max_decoding_steps = 20
+        self.PAD_token = 0
+        self.UNK_token = 1
+        self.EOS_token = 3
+        self.BOS_token = 2
+
+        self.enc_emb = nn.Embedding(
+            self.in_embeddng_size, 
+            self.hidden_size,
+            padding_idx=self.PAD_token
+        )
+
+        self.enc_gru = nn.GRU(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=1,
+            bias=True,
+            batch_first=True,
+            bidirectional=False,
+        )
+
+
+        self.dec_emb = nn.Embedding(
+            self.out_embedding_size, 
+            self.hidden_size,
+            padding_idx=self.PAD_token,
+        )
+
+        self.dec_gru = nn.GRU(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=1,
+            bias=True,
+            batch_first=True,
+            bidirectional=False,
+        )
+
+        self.fc = nn.Linear(
+            in_features=self.hidden_size, 
+            out_features=self.out_embedding_size,
+            bias=True
+        )
+
+
+    def forward(
+        self, 
+        enc_input: torch.Tensor
+    ) -> torch.Tensor:
+        assert enc_input.size(0) == 1, "decoding allowed with 1 sindle word at time"
+        num_batch = 1
+
+        enc_h = torch.zeros((1, num_batch, self.hidden_size))
+
+        enc = self.enc_emb(enc_input)
+        enc_output, enc_h = self.enc_gru(enc, enc_h)
+
+
+        dec_input = torch.tensor([self.BOS_token]).unsqueeze(0).long()
+        dec_h = enc_h
+        preds = torch.zeros(self.max_decoding_steps).long()
+        
+        for i in range(self.max_decoding_steps):
+            dec = self.dec_emb(dec_input)
+            dec_output, dec_h = self.dec_gru(dec, dec_h)
+
+            logit = self.fc(dec_output)
+            topv, topi = logit.data.topk(1, dim=-1)
+            if topi.item() == self.EOS_token:
+                break
+            else:
+                preds[i] = topi.item()
+                dec_input = topi.detach().squeeze(0)
+
+        return preds
